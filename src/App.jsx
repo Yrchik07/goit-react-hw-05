eecb18d63bc9a02276c16e09dccd7893497c169f
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-04-10 16:52:29 +0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,90 @@
+
+import { NavLink, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
+import clsx from "clsx";
+import css from "./App.module.css"
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+const getNavLinkClassName = ({ isActive }) =>
+  clsx(css.navLink, {
+    [css.active]: isActive,
+  });
+
+const App = () => {
+  return (
+    <div>
+    <header>
+      <nav className={css.nav}>
+        <NavLink className={getNavLinkClassName} to="/">
+          Home
+        </NavLink>
+        <NavLink className={getNavLinkClassName} to="/movies">
+        Movies
+        </NavLink>
+        <NavLink className={getNavLinkClassName} to="/movies/:movieId">
+          Products
+        </NavLink>
+        <NavLink className={getNavLinkClassName} to="/movies/:movieId/cast">
+          Search
+        </NavLink>
+        <NavLink className={getNavLinkClassName} to="/movies/:movieId/reviews">
+          Search
+        </NavLink>
+      </nav>
+    </header>
+    {/* URL -> localhost:5123/products/4 */}
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+        <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </main>
+  </div>
+
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import { useEffect, useState } from 'react';
 // import SearchBar from './components/SearchBar/SearchBar';
 // import { requestPhotosByQuery } from './services/api';
@@ -80,10 +167,3 @@
 // export default App;
 
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
